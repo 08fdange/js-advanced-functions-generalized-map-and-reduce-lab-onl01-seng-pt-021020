62cabbb7a654e621a0ccb18af339875d80812e0f
08fdange <08fdange@gmail.com>
2020-09-01 04:32:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your functions here
+function map(array, fn) {
+    return array.map(e => {
+        return fn(e);
+    })
+}
+
+function reduce(array, fn, integer) {
+    let value = (!!integer) ? integer : array[0];
+    let i = (!!integer) ? 0 : 1;
+    for (; i < array.length; i++) {
+        value = fn(array[i], value)
+    }
+    return value;
+}
